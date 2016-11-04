5bec611be2b2a1a2c01c76e74dcb79c2de371f20
0000 <fenglu@nacta.edu.cn>
2016-11-04 16:25:36 +0800

    add make eclipse project to run_mvn

--- run_mvn.sh
+++ run_mvn.sh
@@ -1,4 +1,7 @@
 # /bin/sh
 
-docker run -it --rm --name guac-maven-proj-pack -v "$PWD":/usr/src/guac-maven -w /usr/src/guac-maven -v "$PWD"/../repository:/root/.m2/repository maven mvn clean package
+docker run -it --rm --name guac-maven-proj-pack -v "$PWD":/usr/src/guac-maven -w /usr/src/guac-maven -v ~/.m2/repository:/root/.m2/repository maven mvn clean package
+
+# make eclipse projects
+#docker run -it --rm --name guac-maven-proj-pack -v "$PWD":/usr/src/guac-maven -w /usr/src/guac-maven -v ~/.m2/repository:/root/.m2/repository maven mvn eclipse:eclipse
 
